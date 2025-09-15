8c91a5b4d2e75158c03adeed9c01c1b9ee82f354
nemtudom2 <szabo.istvan@diak.szbi-pg.hu>
2025-09-15 14:59:39 +0200

    init

---
+++ gokart/gokart/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Diagnostics.Tracing;
+
+Hashtable info = new Hashtable();
+info["nev"] = "kinyirakanyar gokart";
+info["cim"] = "Kiskunmajsa amerre nem jár rendőr";
+info["telefon"] = "+36301234567";
+info["weboldal"] = "kinyirakanyar.gov.hu";
+
+List<String> vezeteknevek = (new StreamReader("vezeteknevek.txt").ReadToEnd()).Replace("\"", " ").Replace(" ", "").Split(",").ToList();
+List<String> keresztnevek = (new StreamReader("keresztnevek.txt").ReadToEnd()).Replace("\"", " ").Replace(" ", "").Split(",").ToList();
+
+for (int i = 0; i < new Random().Next(1,151); i++) {
+
+}
+
+class pilota {
+    string nev;
+    int eletkor;
+    DateTime szuletesiido;
+    bool nagykoru;
+    string versenyazonosito;
+
+    string Ekezettelen() {
+        foreach (string s in nev)
+        {
+        }
+    }
+}
