ea2297ea4bf4560f76ab16a83a6f5a61f088cff8
Szabo Istvan <pisti4395@gmail.com>
2025-09-30 23:40:30 +0200

    folytatas, menu atdolgozasa hogy altalanosan hasznalhato legyen, kiiratas es berles elkezdve

--- gokart/gokart/Menu.cs
+++ gokart/gokart/Menu.cs
@@ -8,41 +8,82 @@ using System.Threading.Tasks;
 
 namespace gokart
 {
-    static class Menu
+  static class Menu
+  {
+    public static Dictionary<string, Action> opciok = new Dictionary<string, Action> {
+      ["kilepes"] = () => {Gokart.running = false;},
+      ["berles"] = () => {Console.Clear(); berles(); Console.ReadKey();Console.Clear();},
+      ["kiiratas"] = () => {Console.Clear(); kiiratas(); Console.ReadKey();Console.Clear();},
+      ["modositas"] = () => {Console.Clear(); modositas(); Console.ReadKey();Console.Clear();},
+      ["azonositok"] = () => {Console.Clear(); versenyazonositok(); Console.ReadKey();Console.Clear();},
+    };
+
+    public static void menu(Dictionary<string, Action> opciok, bool running)
     {
-        static Dictionary<string, Action> opciok = new Dictionary<string, Action> {
-            ["berles"] = () => { berles(); },
-            ["kiiratas"] = () => { kiiratas(); },
-            ["modositas"] = () => { modositas(); },
-            ["helytelen"] = () => { Console.WriteLine("nincs ilyen opció vagy helytelen bemenet"); }, };
-        public static void menu()
+      while (running) {
+        Console.Clear();
+        for (int i=0; i<opciok.Keys.Count(); i++)
         {
-            while (true) {
-                Console.WriteLine("mit szeretne tenni: ");
-                foreach (var item in opciok.Keys)
-                {
-                    Console.WriteLine(item);
-                }
-                string input = Console.ReadLine() ?? "helytelen";
-                if (opciok.ContainsKey(input))
-                {
-                    opciok[input]();
-                    break;
-                } else { opciok["helytelen"](); }
-            }
+          Console.WriteLine($"{i} - {opciok.Keys.ToList()[i]}");
         }
-
-        public static void berles()
-        {
-
+        string input = Console.ReadKey().KeyChar.ToString();
+        try{
+          int valasztas = Convert.ToInt32(input);
+          opciok[opciok.Keys.ToList()[valasztas]]();
+        } catch {
         }
-        public static void kiiratas()
-        {
-
-        }
-        public static void modositas()
-        {
-
-        }
+      }
     }
+
+    public static void versenyazonositok(){
+      Console.WriteLine(String.Join(", ", Gokart.pilotak.Select(x => x.versenyazonosito)));
+    }
+
+    public static void berles()
+    {
+      Console.Write("versenyazonosito: ");
+      string azonosito = Console.ReadLine() ?? "";
+      if (!Gokart.pilotak.Exists(x => x.versenyazonosito == azonosito)){
+        Console.WriteLine("nincs ilyen versenyazonosito (nyomjon meg valamit a visszalepeshez)");
+        Console.ReadKey();
+        return;
+      }
+      Console.WriteLine("melyik napra szeretne foglalni? ");
+      List<List<idopont>> futamok = szabad_futamok();
+      Dictionary<string, Action> futamopciok = new Dictionary<string, Action>();
+      foreach (var day in futamok){
+        futamopciok[day[0].nap.ToString()] = () => {Console.WriteLine("asdffds");};
+      }
+
+      string nap = Console.ReadLine()??"";
+      try {
+        menu(futamopciok);
+      }catch{
+      }
+    }
+
+    public static void kiiratas()
+    {
+
+    }
+
+    public static void modositas()
+    {
+
+    }
+    
+    static List<List<idopont>> szabad_futamok(){
+      var returning = new List<List<idopont>>();
+      Gokart.idopontok.ForEach(x=>{
+        List<idopont> nap = new List<idopont>();
+        x.ForEach(y=>{
+          if (y.pilotak.Count()<20){
+            nap.Add(y);
+          }
+        });
+        if (nap.Count>0){returning.Add(nap);}
+      });
+      return returning;
+    }
+  }
 }
--- gokart/gokart/Program.cs
+++ gokart/gokart/Program.cs
@@ -3,115 +3,125 @@ using System.Diagnostics.Tracing;
 
 namespace gokart
 {
-    class Gokart
+  public static class Gokart
+  {
+    static public string Ekezettelenit(string nev)
     {
-        static public string Ekezettelenit(string nev)
+      Hashtable betuk = new Hashtable();
+      betuk['é'] = 'e';
+      betuk['É'] = 'E';
+      betuk['á'] = 'a';
+      betuk['Á'] = 'A';
+      betuk['ö'] = 'o';
+      betuk['Ö'] = 'O';
+      betuk['ő'] = 'o';
+      betuk['Ő'] = 'O';
+      betuk['ó'] = 'o';
+      betuk['Ó'] = 'O';
+      betuk['ú'] = 'u';
+      betuk['Ú'] = 'U';
+      betuk['ű'] = 'u';
+      betuk['Ű'] = 'U';
+      betuk['ü'] = 'u';
+      betuk['Ü'] = 'U';
+      betuk['í'] = 'i';
+      betuk['Í'] = 'I';
+      string returning = "";
+      for (int i = 0; i < nev.Length; i++)
+      {
+        if (betuk.ContainsKey(nev[i]))
         {
-            Hashtable betuk = new Hashtable();
-            betuk['é'] = 'e';
-            betuk['É'] = 'E';
-            betuk['á'] = 'a';
-            betuk['Á'] = 'A';
-            betuk['ö'] = 'o';
-            betuk['Ö'] = 'O';
-            betuk['ő'] = 'o';
-            betuk['Ő'] = 'O';
-            betuk['ó'] = 'o';
-            betuk['Ó'] = 'O';
-            betuk['ú'] = 'u';
-            betuk['Ú'] = 'U';
-            betuk['ű'] = 'u';
-            betuk['Ű'] = 'U';
-            betuk['ü'] = 'u';
-            betuk['Ü'] = 'U';
-            betuk['í'] = 'i';
-            betuk['Í'] = 'I';
-            string returning = "";
-            for (int i = 0; i < nev.Length; i++)
-            {
-                if (betuk.ContainsKey(nev[i]))
-                {
-                    returning += betuk[nev[i]];
-                }
-                else
-                {
-                    returning += nev[i];
-                }
-            }
-            return returning;
+          returning += betuk[nev[i]];
         }
-        static void Main(string[] args)
+        else
         {
-            Hashtable info = new Hashtable();
-            info["nev"] = "kinyirakanyar gokart";
-            info["cim"] = "Kiskunmajsa amerre nem jár rendőr";
-            info["telefon"] = "+36301234567";
-            info["weboldal"] = "kinyirakanyar.gov.hu";
-
-            List<String> vezeteknevek = (new StreamReader("vezeteknevek.txt").ReadToEnd()).Replace("\'", " ").Replace(" ", "").Split(",").ToList();
-            List<String> keresztnevek = (new StreamReader("keresztnevek.txt").ReadToEnd()).Replace("\'", " ").Replace(" ", "").Split(",").ToList();
-
-            List<pilota> pilotak = new List<pilota>();
-            Random random = new Random();
-
-            for (int i = 0; i < new Random().Next(1, 151); i++)
-            {
-                DateTime start = DateTime.Now.AddYears(-16); // min 16 eves
-                DateTime end = start.AddYears(-80); // max 96 
-                int range = (start - end).Days;
-                DateTime szulido = end.AddDays(random.Next(range));
-                string vez = vezeteknevek[new Random().Next(vezeteknevek.Count)];
-                string ker = keresztnevek[new Random().Next(keresztnevek.Count)];
-                pilota ujpilota = new pilota
-                {
-                    vezeteknev = vez,
-                    keresztnev = ker,
-                    szuletesiido = szulido,
-                    eletkor = DateTime.Now.Year - szulido.Year,
-                    nagykoru = DateTime.Now.Year - szulido.Year > 18,
-                    versenyazonosito = $"GO-{Ekezettelenit(vez)}{Ekezettelenit(ker)}-{szulido.Year}{szulido.Month}{szulido.Day}",
-                    email = $"{Ekezettelenit(vez)}.{Ekezettelenit(ker)}@gmail.com"
-                };
-            }
-
-            List<List<idopont>> idopontok = new List<List<idopont>>();
-            for (int i = 0; i < 9; i++)
-            {
-                List<idopont> nap = new List<idopont>();
-                for (int j = 0; j < 11; j++)
-                {
-                    nap.Add(new idopont
-                    {
-                        pilotak = new List<pilota>(),
-                    });
-                }
-            }
-            Menu.menu();
+          returning += nev[i];
         }
+      }
+      return returning;
     }
 
-    public class idopont
-    {
-        public List<pilota> pilotak;
-        //public DateTime date;
-        public void foglalas(pilota pilot)
-        {
-            this.pilotak.Add(pilot);
-        }
-    }
-    public class pilota
-    {
-        public string vezeteknev;
-        public string keresztnev;
-        public int eletkor;
-        public DateTime szuletesiido;
-        public bool nagykoru;
-        public string versenyazonosito;
-        public string email;
+    public static List<pilota> pilotak = new List<pilota>();
+    public static bool running = true;
+    public static List<List<idopont>> idopontok = new List<List<idopont>>();
 
-        public string ekezettelen()
+    static void Main(string[] args)
+    {
+      Hashtable info = new Hashtable();
+      info["nev"] = "kinyirakanyar gokart";
+      info["cim"] = "Kiskunmajsa, amerre nem jár rendőr";
+      info["telefon"] = "+36301234567";
+      info["weboldal"] = "kinyirakanyar.gov.hu";
+
+      List<String> vezeteknevek = (new StreamReader("vezeteknevek.txt").ReadToEnd()).Replace("\n", "").Replace("\'", " ").Replace(" ", "").Split(",").ToList();
+      List<String> keresztnevek = (new StreamReader("keresztnevek.txt").ReadToEnd()).Replace("\n", "").Replace("\'", " ").Replace(" ", "").Split(",").ToList();
+
+      Random random = new Random();
+
+      for (int i = 0; i < new Random().Next(15, 151); i++)
+      {
+        DateTime start = DateTime.Now.AddYears(-16); // min 16 eves
+        DateTime end = start.AddYears(-80); // max 96 
+        int range = (start - end).Days;
+        DateTime szulido = end.AddDays(random.Next(range));
+        string vez = vezeteknevek[new Random().Next(vezeteknevek.Count)];
+        string ker = keresztnevek[new Random().Next(keresztnevek.Count)];
+        pilota ujpilota = new pilota
         {
-            return Gokart.Ekezettelenit(vezeteknev) + " " + Gokart.Ekezettelenit(keresztnev);
+          vezeteknev = vez,
+          keresztnev = ker,
+          szuletesiido = szulido,
+          eletkor = DateTime.Now.Year - szulido.Year,
+          nagykoru = DateTime.Now.Year - szulido.Year > 18,
+          versenyazonosito = $"GO-{Ekezettelenit(vez)}{Ekezettelenit(ker)}-{szulido.Year}{szulido.Month}{szulido.Day}",
+          email = $"{Ekezettelenit(vez)}.{Ekezettelenit(ker)}@gmail.com"
+        };
+        pilotak.Add(ujpilota);
+      }
+      pilotak.Add(new pilota());
+
+      for (int i = 0; i < 9; i++)
+      {
+        List<idopont> nap = new List<idopont>();
+        for (int j = 0; j < 11; j++)
+        {
+          nap.Add(new idopont
+            {
+              pilotak = new List<pilota>(),
+              start = 8+j,
+              nap = i+1,
+            }
+          );
         }
+      }
+      Console.WriteLine("mit szeretne tenni: ");
+      Menu.menu(Menu.opciok, running);
     }
+  }
+
+  public class idopont
+  {
+    public List<pilota> pilotak = new List<pilota>();
+    public int start;
+    public int nap;
+    public void foglalas(pilota pilot)
+    {
+      this.pilotak.Add(pilot);
+    }
+  }
+  public class pilota
+  {
+    public string vezeteknev = "teszt";
+    public string keresztnev = "teszt";
+    public int eletkor;
+    public DateTime szuletesiido = DateTime.Now;
+    public bool nagykoru;
+    public string versenyazonosito = "teszt";
+    public string email = "teszt";
+
+    public string ekezettelen()
+    {
+      return Gokart.Ekezettelenit(vezeteknev) + " " + Gokart.Ekezettelenit(keresztnev);
+    }
+  }
 }
